b8012d0578abd1b4d6c74ce85a0d94bd98eb41d0
Zike Cao <75461603+ZikeCao@users.noreply.github.com>
2023-05-10 23:27:08 +0800

    Delete USER/project_car （蓝牙串口2之前） directory

--- USER/project_car （蓝牙串口2之前）/src/bluetooth.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BLUETOOTH_H
-#define __BLUETOOTH_H
-
-void debug_usart();
-void usart1_send_byte(char data);
-// char usart1_recv_byte(void);
-
-#endif
--- USER/project_car （蓝牙串口2之前）/src/red.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RED_H
-#define __RED_H
-#define Red1 GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_6) // 右边红外1号
-#define Red2 GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_5) // 左边红外4号
-
-void red_init(void);
-
-#endif
-
